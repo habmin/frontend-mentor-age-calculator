63f003cdb013c642272a2488279efeb8c983272a
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2023-04-25 02:06:59 -0400

    Input validation andd error display for Year

--- js/calculation.js
+++ js/calculation.js
@@ -1,5 +1,12 @@
 const currentDate = new Date();
+
 const yearInput = document.getElementById("year");
+const monthInput = document.getElementById("month");
+const dayInput = document.getElementById("day");
+
+const yearError = document.getElementById("year-error");
+const monthError = document.getElementById("month-error");
+const dayError = document.getElementById("day-error");
 
 const verifyDayInput = () => {
 
@@ -10,12 +17,26 @@ const verifyMonthInput = () => {
 };
 
 const verifyYearInput = () => {
-    if (yearInput.value == "")
-        console.log("Empty");
-    else if (!yearInput.checkValidity())
-        console.log("Not a valid number");
-    else if (yearInput.value < 1 || yearInput.value > currentDate.getFullYear())
-        console.log("Must be in the past");
+    console.log(yearInput.checkValidity(), yearInput.value);
+    console.log(yearInput.checkValidity() && yearInput.value !== "" && (yearInput.value < 1 || yearInput.value > currentDate.getFullYear()));
+    // Valid States
+    // 1. checkValidity and empty
+    // 2. check Vallidity and in range
+    // Invalid states
+    // 1. check validity fails (non-number in input)
+    
+    // 2. check validity passes but input in not in range
+    if (yearInput.checkValidity() && yearInput.value !== "" && (yearInput.value < 1 || yearInput.value > currentDate.getFullYear())) {
+        yearError.innerHTML = "Must be in the past";
+        yearError.style.visibility = "visible";
+    }
+    else if (yearInput.checkValidity()) {
+        yearError.style.visibility = "hidden";
+    }
+    else if (!yearInput.checkValidity()) {
+        yearError.innerHTML = "Not a valid number";
+        yearError.style.visibility = "visible";
+    }
 };
 
 const submitCalculation = () => {
